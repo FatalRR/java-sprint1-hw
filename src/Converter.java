16a202b66e27a9ee85f327544856643871315596
Sergey Malyunov <FatalRR@yandex.ru>
2022-09-22 14:52:57 +0700

    убрал поле static у методов внутри класса

--- src/Converter.java
+++ src/Converter.java
@@ -2,10 +2,10 @@ public class Converter {
     static final double oneStepLength=0.00075;
     static final  double oneStepCCal=0.05;
 
-    static double convertDist(double sumSteps) {
+    double convertDist(double sumSteps) {
         return sumSteps * oneStepLength;
     }
-    static double convertCCal(double sumSteps) {
+    double convertCCal(double sumSteps) {
         return sumSteps * oneStepCCal;
     }
 }
